0d3a57e92e0be7ab81490a6c99060898736b298b
beltracr <cristiabeltran0628@gamil.com>
2024-06-14 16:09:37 -0500

    actualizacion

---
+++ script/abrir-menu.js
@@ -0,0 +1,6 @@
+const navlist = document.querySelector(".nav-list");
+const abrirmenu = document.querySelector(".abrir-menu");
+
+
+abrirmenu.addEventListener("click", () =>{ navlist.classList.toggle("active") })
+
